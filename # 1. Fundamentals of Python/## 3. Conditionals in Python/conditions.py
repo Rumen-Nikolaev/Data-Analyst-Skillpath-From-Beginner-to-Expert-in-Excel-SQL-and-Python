82443b018ca9de93b9dabcef32323c9f0f51c75b
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-10-18 00:46:01 +0300

    Create conditions.py

---
+++ # 1. Fundamentals of Python/## 3. Conditionals in Python/conditions.py
@@ -0,0 +1,34 @@
+age = int(input("Please enter your age: "))
+if age < 21:
+    print("You can't enter this place!")
+Please enter your age: 22
+number = int(input("Please enter a number: "))
+if number < 0:
+    print("Your number is negative.")
+Please enter a number: 1
+age = int(input("Please enter your age: "))
+if age < 21:
+    print("You can't enter this place!")
+else:
+    print("Welcome")
+Please enter your age: 22
+Welcome
+number = int(input("Please enter a number: "))
+if number < 0:
+    print("Your number is negative.")
+else:
+    print("Your number is positive.")
+Please enter a number: 3
+Your number is positive.
+Model = int(input("Please choose 1-3: "))
+
+if Model == 1:
+    print("You choose Rolls-Royce.")
+elif Model == 2:
+    print("You choose Aston Martin.")
+elif Model == 3:
+    print("You choose Bentley.")
+else:
+    print("Invalid Number!")
+Please choose 1-3: 1
+You choose Rolls-Royce.
