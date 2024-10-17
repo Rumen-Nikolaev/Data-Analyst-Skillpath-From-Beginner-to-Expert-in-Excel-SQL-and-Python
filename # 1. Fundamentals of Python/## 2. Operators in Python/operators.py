7b6708fb9d203046635cc3a75817c46a0e80bd23
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-10-18 00:21:07 +0300

    Create operators.py

---
+++ # 1. Fundamentals of Python/## 2. Operators in Python/operators.py
@@ -0,0 +1,43 @@
+A = 9
+B = 15
+A + B
+24
+B - A
+6
+A / B
+0.6
+B % A
+6
+B ** 2
+225
+A = True
+type(A)
+bool
+B = False
+type(B)
+bool
+A = 9
+B = 15
+A == B
+False
+A != B
+True
+A < B
+True
+A > B
+False
+A <= B
+True
+A >= B
+False
+C = 1
+C += 1
+C -= 2
+C
+0
+A == 9 and B == 15
+True
+A == 15 or B == 15
+True
+not B == 15
+False
