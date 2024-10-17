939f2788fcb4e635cf773e08a6a2df2fcdd09b14
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-10-18 01:59:10 +0300

    Create functions.py

---
+++ # 1. Fundamentals of Python/## 8. Functions in Python/functions.py
@@ -0,0 +1,26 @@
+def Sum(A,B):
+    """This function sum the variables"""
+    print(A + B)
+Sum(4,5)
+9
+def Sum(A,B,C,D):
+    print(A+B+C+D)
+def Multiply(A):
+    print(A*2)
+Sum(3,4,5,6)
+18
+Multiply(5)
+10
+def Sum(A,B,C,D):
+    return A+B+C+D
+def Multiply(A):
+    return A*2
+Multiply(Sum(10,12,14,13))
+98
+def Sum(a,b):
+    return a + b
+Sum(10,14)
+24
+Sum_1 = lambda a,b : a+b
+Sum_1(10,14)
+24
