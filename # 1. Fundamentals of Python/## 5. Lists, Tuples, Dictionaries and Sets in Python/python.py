4224f4dba922ad08a3ee00d8c8946f085f824984
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-10-18 01:18:58 +0300

    Create python.py

---
+++ # 1. Fundamentals of Python/## 5. Lists, Tuples, Dictionaries and Sets in Python/python.py
@@ -0,0 +1,41 @@
+MyList = [1,2,3,4,5,6,7,8]
+type(MyList)
+list
+MyList_2 = [1,2,3,"apple","banana",13,25,'a','b',13.5]
+type(MyList_2)
+list
+MyList_3 = [1,2,3,[1,2,3],"apple","banana",["apple","banana"]]
+type(MyList_3)
+list
+MyTuple_3 = (1,2,3,[1,2,3],"apple","banana",["apple","banana"])
+type(MyTuple_3)
+tuple
+MyTuple_4 = (5)
+type(MyTuple_4)
+int
+MyTuple_5 = (5,)
+type(MyTuple_5)
+tuple
+MyDict = {"Zero": 0, "One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
+type(MyDict)
+dict
+MyDict_E = {}
+type(MyDict_E)
+dict
+MyDict_2 = {"January": 31, "February": 28, "March": 31}
+type(MyDict_2)
+dict
+MyDict_3 = {"Name": "John", "Surname": "Doe", "Job": "Unemployed", "Job": "Software Developer"}
+type(MyDict_3)
+dict
+print("MyDict_3['Job']: ", MyDict_3["Job"])
+MyDict_3['Job']:  Software Developer
+MySet = {1,2,3,4,5}
+type(MySet)
+set
+MySet_2 = {"a","b","c","d","e"}
+type(MySet_2)
+set
+MySet_3 = {1,2,2,3,3,3,4,4,5}
+MySet_3
+{1, 2, 3, 4, 5}
