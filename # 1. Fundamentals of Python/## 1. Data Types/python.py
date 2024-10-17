e0eb05e4d1551a83ae8d9b2dcdd8cf0fe991923f
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-10-18 00:14:57 +0300

    Create python.py

---
+++ # 1. Fundamentals of Python/## 1. Data Types/python.py
@@ -0,0 +1,45 @@
+A = 10
+Name = "John"
+Miles = 130.7
+City_Name = "London"
+type(A)
+int
+type(Name)
+str
+type(City_Name)
+str
+type(Miles)
+float
+a = 10
+type(a)
+int
+b = 99.9
+type(b)
+float
+c = 45.j
+type(c)
+complex
+"Python"
+'Python'
+"Python'
+  Cell In[18], line 1
+    "Python'
+    ^
+SyntaxError: unterminated string literal (detected at line 1)
+print("Welcome Python")
+Welcome Python
+Name = input("What's your name?")
+What's your name?John
+Name
+'John'
+Variable_1 = 100
+type(Variable_1)
+int
+float(Variable_1)
+100.0
+type(float(Variable_1))
+float
+str(Variable_1)
+'100'
+type(str(Variable_1))
+str
