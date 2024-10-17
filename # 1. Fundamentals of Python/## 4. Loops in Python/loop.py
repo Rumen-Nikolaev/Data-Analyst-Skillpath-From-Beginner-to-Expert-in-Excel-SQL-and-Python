77c99614ea6d715d2c14559a8144946d1b569b96
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-10-18 01:00:37 +0300

    Create loop.py

---
+++ # 1. Fundamentals of Python/## 4. Loops in Python/loop.py
@@ -0,0 +1,119 @@
+i = 0
+while i < 15:
+    print(i)
+    i += 1
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+i = 0
+while i < 20:
+    print("Hello World")
+    i += 1
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+for characters in "Python":
+    print(characters)
+P
+y
+t
+h
+o
+n
+Number = 0
+while Number < 15:
+    print(Number)
+    if Number == 9:
+        break
+    Number += 1
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+Number = 0
+while Number < 20:
+    if Number == 2:
+        Number += 1
+        continue
+    print(Number)
+    Number += 1
+0
+1
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+
+range(0,15)
+range(0, 15)
+print(*range(0,15))
+0 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+print(*range(15))
+0 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+print(*range(3,30,4))
+3 7 11 15 19 23 27
+for number in range(0,15):
+    print(number)
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
