2103b17e11bedb6e3c4f6f30fcbf45e3b6d872f4
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-10-18 18:33:00 +0300

    Create slicing.py

---
+++ ## 3. Numpy for Data Manipulation/### 19. Slicing One-Dimensional Numpy Arrays/slicing.py
@@ -0,0 +1,10 @@
+import numpy as np
+example = np.arange(10, 20)
+example
+array([10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
+example[2:6]
+array([12, 13, 14, 15])
+example[3::2]
+array([13, 15, 17, 19])
+example[:7:3]
+array([10, 13, 16])
