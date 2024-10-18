d1891b2ee3f1b0ffa65267814349ecaf1ed1d9a3
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-10-18 20:37:00 +0300

    Create statistical.py

---
+++ ## 3. Numpy for Data Manipulation/### 29. Statistical Operations in Numpy/statistical.py
@@ -0,0 +1,20 @@
+import numpy as np
+array = np.arange(1, 11)
+array
+array([ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10])
+np.sum(array)
+55
+np.mean(array)
+5.5
+np.median(array)
+5.5
+np.min(array)
+1
+np.max(array)
+10
+np.std(array)
+2.8722813232690143
+np.var(array)
+8.25
+np.sqrt(np.var(array))
+2.8722813232690143
