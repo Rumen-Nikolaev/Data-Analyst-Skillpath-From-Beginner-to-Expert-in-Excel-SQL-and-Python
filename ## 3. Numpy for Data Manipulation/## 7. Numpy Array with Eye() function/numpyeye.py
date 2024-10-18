208271134ba9bcd39febfa6ce81a445020862292
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-10-18 14:34:01 +0300

    Create numpyeye.py

---
+++ ## 3. Numpy for Data Manipulation/## 7. Numpy Array with Eye() function/numpyeye.py
@@ -0,0 +1,13 @@
+import numpy as np
+np.eye(3)
+array([[1., 0., 0.],
+       [0., 1., 0.],
+       [0., 0., 1.]])
+np.eye(3, 2)
+array([[1., 0.],
+       [0., 1.],
+       [0., 0.]])
+np.eye(3, 2, dtype = "bool")
+array([[ True, False],
+       [False,  True],
+       [False, False]])
