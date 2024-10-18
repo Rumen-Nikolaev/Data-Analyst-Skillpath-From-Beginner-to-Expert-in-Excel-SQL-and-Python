a02cbad554a9d94ae1e881c1dcd3e60c6eef710b
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-10-18 18:30:04 +0300

    Create indexing.py

---
+++ ## 3. Numpy for Data Manipulation/### 18. Indexing Numpy Arrays/indexing.py
@@ -0,0 +1,19 @@
+import numpy as np
+array = np.arange(0, 11)
+array
+array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10])
+array[4]
+4
+array[-2]
+9
+variable = np.array([[5, 10, 15], [20, 25, 30], [35, 40, 45]])
+variable
+array([[ 5, 10, 15],
+       [20, 25, 30],
+       [35, 40, 45]])
+variable[0, 0]
+5
+variable[2, 1]
+40
+variable[-1, -3]
+35
