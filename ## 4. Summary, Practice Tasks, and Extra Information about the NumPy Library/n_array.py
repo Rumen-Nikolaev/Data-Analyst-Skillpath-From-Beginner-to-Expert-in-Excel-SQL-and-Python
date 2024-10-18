f28795a1790e83c2d9cfa04b0923805aa119edeb
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-10-18 21:11:44 +0300

    Create n_array.py

---
+++ ## 4. Summary, Practice Tasks, and Extra Information about the NumPy Library/n_array.py
@@ -0,0 +1,13 @@
+import numpy as np
+N_Array = np.array([[1,2,3],[4,5,6]])
+type(N_Array)
+numpy.ndarray
+N_Array
+array([[1, 2, 3],
+       [4, 5, 6]])
+N_Array_1 = np.array([[2,4,6],[9,8,15]])
+type(N_Array_1)
+numpy.ndarray
+N_Array_1
+array([[ 2,  4,  6],
+       [ 9,  8, 15]])
